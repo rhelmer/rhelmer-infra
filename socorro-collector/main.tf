3654df972abc3e67a9beaedf27b58bbaf95f3da3
Robert Helmer <rhelmer@rhelmer.org>
2015-04-14 18:42:54 -0700

    make this socorro-collector specifically

--- socorro-collector/main.tf
+++ socorro-collector/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
     secret_key = "${var.secret_key}"
 }
 
-resource "aws_security_group" "any_to_socorro__ssh" {
-    name = "${var.environment}__any_to_socorro__ssh"
-    description = "Allow (alt) SSH to the Socorro node."
+resource "aws_security_group" "any_to_socorro-collector__ssh" {
+    name = "${var.environment}__any_to_socorro-collector__ssh"
+    description = "Allow (alt) SSH to the Socorro Collector node."
     ingress {
         from_port = "${var.alt_ssh_port}"
         to_port = "${var.alt_ssh_port}"
@@ -20,8 +20,8 @@ resource "aws_security_group" "any_to_socorro__ssh" {
     }
 }
 
-resource "aws_security_group" "internet_to_socorro_elb__http" {
-    name = "${var.environment}__internet_to_socorro_elb__http"
+resource "aws_security_group" "internet_to_socorro-collector_elb__http" {
+    name = "${var.environment}__internet_to_socorro-collector_elb__http"
     description = "Allow incoming traffic from Internet to HTTP on ELBs."
     ingress {
         from_port = 80
@@ -36,15 +36,15 @@ resource "aws_security_group" "internet_to_socorro_elb__http" {
     }
 }
 
-resource "aws_security_group" "elb_to_socorro__http" {
-    name = "${var.environment}__elb_to_socorro__http"
-    description = "Allow HTTP from ELBs to socorro."
+resource "aws_security_group" "elb_to_socorro-collector__http" {
+    name = "${var.environment}__elb_to_socorro-collector__http"
+    description = "Allow HTTP from ELBs to the Socorro Collector nodes."
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
         security_groups = [
-            "${aws_security_group.internet_to_socorro_elb__http.id}"
+            "${aws_security_group.internet_to_socorro-collector_elb__http.id}"
         ]
     }
     tags {
@@ -52,8 +52,8 @@ resource "aws_security_group" "elb_to_socorro__http" {
     }
 }
 
-resource "aws_elb" "elb_for_socorro" {
-    name = "${var.environment}--elb-for-socorro"
+resource "aws_elb" "elb_for_socorro-collector" {
+    name = "${var.environment}--elb-for-socorro-collector"
     availability_zones = [
         "${var.region}a",
         "${var.region}b"
@@ -65,25 +65,25 @@ resource "aws_elb" "elb_for_socorro" {
         lb_protocol = "http"
     }
     security_groups = [
-        "${aws_security_group.internet_to_socorro_elb__http.id}"
+        "${aws_security_group.internet_to_socorro-collector_elb__http.id}"
     ]
 }
 
-resource "aws_launch_configuration" "lc_for_socorro_asg" {
-    name = "${var.environment}__lc_for_socorro_asg"
+resource "aws_launch_configuration" "lc_for_socorro-collector_asg" {
+    name = "${var.environment}__lc_for_socorro-collector_asg"
     image_id = "${lookup(var.base_ami, var.region)}"
     instance_type = "t2.micro"
     key_name = "${lookup(var.ssh_key_name, var.region)}"
     iam_instance_profile = "generic"
     associate_public_ip_address = true
     security_groups = [
-        "${aws_security_group.elb_to_socorro__http.id}",
-        "${aws_security_group.any_to_socorro__ssh.id}"
+        "${aws_security_group.elb_to_socorro-collector__http.id}",
+        "${aws_security_group.any_to_socorro-collector__ssh.id}"
     ]
 }
 
-resource "aws_autoscaling_group" "asg_for_socorro" {
-    name = "${var.environment}__asg_for_socorro"
+resource "aws_autoscaling_group" "asg_for_socorro-collector" {
+    name = "${var.environment}__asg_for_socorro-collector"
     availability_zones = [
         "${var.region}a",
         "${var.region}b",
@@ -91,13 +91,13 @@ resource "aws_autoscaling_group" "asg_for_socorro" {
     ]
     vpc_zone_identifier = ["${split(",", var.subnets)}"]
     depends_on = [
-        "aws_launch_configuration.lc_for_socorro_asg"
+        "aws_launch_configuration.lc_for_socorro-collector_asg"
     ]
-    launch_configuration = "${aws_launch_configuration.lc_for_socorro_asg.id}"
+    launch_configuration = "${aws_launch_configuration.lc_for_socorro-collector_asg.id}"
     max_size = 1
     min_size = 1
     desired_capacity = 1
     load_balancers = [
-        "${var.environment}--elb-for-socorro"
+        "${var.environment}--elb-for-socorro-collector"
     ]
 }
